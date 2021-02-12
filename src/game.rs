3ff44b874c3a30e5fc87d1fe72d39b3a63f7ec5c
Eduardo San Martin Morote <posva13@gmail.com>
2021-02-12 09:57:54 +0100

    some tests

--- src/game.rs
+++ src/game.rs
@@ -50,6 +50,7 @@ pub struct FieldCell {
 pub struct Field {
     pub config: GameConfig,
     cells: Vec<FieldCell>,
+    bombs: HashSet<usize>,
 }
 
 impl Field {
@@ -59,18 +60,17 @@ impl Field {
         let mut field = Field {
             config: config.clone(),
             cells: Vec::with_capacity(size),
+            bombs: HashSet::new(),
         };
 
-        let mut bombs: HashSet<usize> = HashSet::new();
-
-        while bombs.len() < config.mines as usize {
-            bombs.insert(rng.gen_range(0, size));
+        while field.bombs.len() < config.mines {
+            field.bombs.insert(rng.gen_range(0, size));
         }
 
         for i in 0..size {
             field.cells.push(FieldCell {
                 revealed: false,
-                cell_type: if bombs.contains(&i) {
+                cell_type: if field.bombs.contains(&i) {
                     FieldCellType::Mine
                 } else {
                     FieldCellType::Empty(0)
@@ -78,73 +78,97 @@ impl Field {
             });
         }
 
-        fn increment_count(field: &mut Field, i: usize) {
-            if let Some(cell) = field.cells.get_mut(i) {
-                if let FieldCellType::Empty(n) = &mut cell.cell_type {
-                    *n += 1;
-                }
+        field.compute_field();
+
+        field
+    }
+
+    fn increment_cell_count(&mut self, i: usize) {
+        if let Some(cell) = self.cells.get_mut(i) {
+            if let FieldCellType::Empty(n) = &mut cell.cell_type {
+                *n += 1;
             }
         }
+    }
 
+    fn compute_field(&mut self) {
         // increment counters
-        for i in 0..size {
-            if bombs.contains(&i) {
+        for i in 0..self.cells.len() {
+            if self.bombs.contains(&i) {
                 // up
-                if i > config.columns {
-                    increment_count(&mut field, i - config.columns);
+                if i > self.config.columns {
+                    self.increment_cell_count(i - self.config.columns);
 
                     // up left
-                    if i % config.columns > 0 {
-                        increment_count(&mut field, i - config.columns - 1);
+                    if i % self.config.columns > 0 {
+                        self.increment_cell_count(i - self.config.columns - 1);
                     }
 
                     // up right
-                    if i / config.rows < config.columns {
-                        increment_count(&mut field, i - config.columns + 1);
+                    if i / self.config.rows < self.config.columns {
+                        self.increment_cell_count(i - self.config.columns + 1);
                     }
                 }
 
                 // down
-                if i / config.columns < config.rows - 1 {
-                    increment_count(&mut field, i + config.columns);
+                if i / self.config.columns < self.config.rows - 1 {
+                    self.increment_cell_count(i + self.config.columns);
 
                     // up left
-                    if i % config.columns > 0 {
-                        increment_count(&mut field, i + config.columns - 1);
+                    if i % self.config.columns > 0 {
+                        self.increment_cell_count(i + self.config.columns - 1);
                     }
 
                     // up right
-                    if i / config.rows < config.columns {
-                        increment_count(&mut field, i + config.columns + 1);
+                    if i / self.config.rows < self.config.columns {
+                        self.increment_cell_count(i + self.config.columns + 1);
                     }
                 }
 
                 // left
-                if i % config.columns > 0 {
-                    increment_count(&mut field, i - 1);
+                if i % self.config.columns > 0 {
+                    self.increment_cell_count(i - 1);
                 }
 
                 // right
-                if i / config.rows < config.columns {
-                    increment_count(&mut field, i + 1);
+                if i / self.config.rows < self.config.columns {
+                    self.increment_cell_count(i + 1);
                 }
             }
         }
-
-        field
     }
 
     pub fn from(field_text: Vec<&str>) -> Field {
         let config = GameConfig {
             rows: field_text.len(),
             columns: field_text.get(0).unwrap().len(),
-            mines: field_text.into_iter().map(|c| c.matches("x").count()).sum(),
+            mines: field_text.iter().map(|c| c.matches("x").count()).sum(),
         };
         let size = (config.rows * config.columns);
+
         let mut field = Field {
             config,
             cells: Vec::with_capacity(size),
+            bombs: HashSet::new(),
         };
+
+        for line in field_text.iter() {
+            for (i, cell) in line.chars().enumerate() {
+                if cell == 'x' {
+                    field.bombs.insert(i);
+                }
+                field.cells.push(FieldCell {
+                    revealed: false,
+                    cell_type: if cell == 'x' {
+                        FieldCellType::Mine
+                    } else {
+                        FieldCellType::Empty(0)
+                    },
+                });
+            }
+        }
+
+        field.compute_field();
 
         field
     }
@@ -187,6 +211,41 @@ impl Field {
             i += 1;
         }
         text.push('\n');
+        text
+    }
+
+    pub fn as_text_ascii(&self, show_all: bool) -> String {
+        let mut i = 0usize;
+        let len = self.cells.len();
+        let mut text = String::new();
+        while i < len {
+            if i > 0 && i % self.config.rows == 0 {
+                // new line
+                text.push('\n');
+            }
+
+            let cell = self.cells.get(i).unwrap();
+
+            if cell.revealed || show_all {
+                match cell.cell_type {
+                    FieldCellType::Mine => {
+                        text.push('x');
+                    }
+                    FieldCellType::Empty(n) => {
+                        if n > 0 {
+                            text.push_str(&n.to_string());
+                        } else {
+                            text.push(' ');
+                        };
+                    }
+                }
+            } else {
+                text.push(' ');
+            }
+
+            i += 1;
+        }
+
         text
     }
 
@@ -262,5 +321,20 @@ mod tests {
         assert_eq!(field.config.mines, 2);
         assert_eq!(field.config.columns, 5);
         assert_eq!(field.config.rows, 4);
+    }
+
+    #[test]
+    fn correct_numbers_simple() {
+        let field = Field::from(vec!["ooooo", "ooxoo", "ooxoo", "ooooo"]);
+
+        assert_eq!(
+            field.as_text_ascii(true),
+            "\
+ 111
+ 2x2
+ 2x2
+ 111\
+"
+        );
     }
 }
